dfc8950d23de200edaf8db9cf7e359096241b59e
Christopher Brown <cmbrown1598@gmail.com>
2020-06-26 15:58:05 -0700

    Update to Slides.

--- 05_if.fsx
+++ 05_if.fsx
@@ -4,4 +4,4 @@ let greet name =
     let hr = System.DateTime.Now.Hour
     if hr < 12 then sprintf "Good morning %s!" name
     elif hr < 17 then sprintf "Good afternoon %s!" name
-    sprintf "Good evening, %s!" name
+    else sprintf "Good evening, %s!" name
